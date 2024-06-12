aae5ee8821b268f99cec500707291827b95b1f49
kcieniawska <14690@eans-nt.edu.pl>
2024-06-12 12:30:26 +0200

    Poprawiono sekcje z opiniami (products)

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,15 @@
 from app import app
-from flask import render_template, request, redirect, url_for
+from app import utils
+from flask import render_template, request, redirect, url_for, flash, jsonify
 import requests
+import os
+import json
 from bs4 import BeautifulSoup
-@app.route('/')
 
+@app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html.jinja')
-
-
 
 @app.route('/about')
 def about():
@@ -18,25 +19,60 @@ def about():
 def home():
     return render_template('base.html.jinja')
 
-@app.route('/extract', methods=['POST','GET'])
+@app.route('/extract', methods=['POST', 'GET'])
 def extract():
     if request.method == "POST":
         product_id = request.form.get("product_id")
-        # walidacja
+        # Walidacja
         url = f"https://www.ceneo.pl/{product_id}"
         response = requests.get(url)
-        if response.status_code == requests.codes['ok']: 
+        if response.status_code == requests.codes.ok: 
             page = BeautifulSoup(response.text, "html.parser")
             opinions_count = page.select_one("a.product-review__link > span")
             if opinions_count:
-                return redirect(url_for('product',product_id=product_id))
-            return render_template('extract.html.jinja', error="Produkt nie posiada opinii!")
-        return render_template('extract.html.jinja', error="Produkt o podanym kodzie nie istnieje!")
-    return render_template('extract.html.jinja')
+                url = f"https://www.ceneo.pl/{product_id}#tab=reviews"
+                all_opinions = []
+                while url:
+                    response = requests.get(url)
+                    page = BeautifulSoup(response.text, "html.parser")
+                    opinions = page.select("div.js_product-review")
+                    for opinion in opinions:
+                        single_opinion = {
+                            key: utils.get_data(opinion, *value)
+                            for key, value in utils.selectors.items()
+                        }
+                        all_opinions.append(single_opinion)
+                    try:
+                        url = "https://ceneo.pl" + page.select_one("a.pagination__next")["href"]
+                    except TypeError:
+                        url = None
+
+                if not os.path.exists("app/data"):
+                    os.mkdir("app/data")
+                if not os.path.exists("app/data/opinions"):
+                    os.mkdir("app/data/opinions")
+
+                with open(f"app/data/opinions/{product_id}.json", "w", encoding="UTF-8") as jf:
+                    json.dump(all_opinions, jf, indent=4, ensure_ascii=False)
+
+                return redirect(url_for('product', product_id=product_id))
+            else:
+                return render_template('extract.html.jinja', error="Produkt nie posiada opinii!")
+        else:
+            return render_template('extract.html.jinja', error="Produkt o podanym kodzie nie istnieje!")
+    else:
+        return render_template('extract.html.jinja')
 
 @app.route('/list')
 def list():
     return render_template('list.html.jinja')
+
 @app.route('/product/<product_id>')
 def product(product_id):
-    return render_template('product.html.jinja', product_id=product_id)
+    opinions_file = f"app/data/opinions/{product_id}.json"
+    if os.path.exists(opinions_file):
+        with open(opinions_file, "r", encoding="UTF-8") as jf:
+            product_opinions = json.load(jf)
+        return render_template('product.html.jinja', product=product_opinions, product_id=product_id)
+    else:
+        return render_template('product.html.jinja', error="Opinie dla tego produktu nie zostały znalezione!", product_id=product_id)
